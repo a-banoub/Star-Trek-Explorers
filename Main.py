3d3cb5f46d542104c4dcaa3b71e27a381774485c
a-banoub <118484970+a-banoub@users.noreply.github.com>
2023-08-03 18:43:17 -0400

    Updated charactersheet pdf template
    -Updated Font Sizes,
    -Adjusted FormFields
    -Added "add_species" function to Character Class

--- Main.py
+++ Main.py
@@ -9,6 +9,9 @@ CharacterName = input("Character Name: ")
 PlayerCharacter = CharacterSheet.Character(CharacterName)	
 print (PlayerCharacter.name)
 
+species = input ("Species Name")
+PlayerCharacter.add_species (species)
+
 archetype_names = list(Archetypes.archetypes.keys())
 combo = PlayerCharacter.call_combobox_archetype_select(PlayerCharacter, "Choose Archetype", archetype_names)
 
