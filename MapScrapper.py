68b2bce7c5dd9a0751e1f4da4f04941197710438
a-banoub <118484970+a-banoub@users.noreply.github.com>
2023-08-08 12:44:51 -0400

    Distance Calc
    TODO: Convert to LY

--- MapScrapper.py
+++ MapScrapper.py
@@ -1,24 +1,44 @@
 import requests
 import bs4
 import json
+import math
 
 filename = 'MapData.json'
 
-planet = input('Planet to Search?')
+currentplanet = 'Sol'
+
+destination = input('Planet to Search?')
+
 
 with open(filename, "r") as file:
+	
 	mapdata = json.load(file)
 	found = False
 	print (mapdata)
 	
 	for feature in mapdata['features']:
 		
-		if feature ["properties"] ["name"] == planet:
-			if feature ["properties"] ["subclass"] == 'major':
+		if feature ["properties"] ["name"] == currentplanet:
 				print("Found by Name:", feature)
-
+				startingcoords = feature['geometry']['coordinates']
+	
+	for feature in mapdata['features']:
+		
+		if feature ["properties"] ["name"] == destination:
+			if feature ["properties"] ["subclass"] == 'major':
+				
+				print("Found by Name:", feature)
+				destinationcoords = feature['geometry']['coordinates']
 				found = True
 				break
-		
-	if not found: 
-		print('Planet not Found')
+		if not found: 
+			print('Planet not Found')
+			
+print ('Starting Coords: ', startingcoords, 'Destination Coords: ', destinationcoords)
+			
+distance = math.sqrt(
+	(destinationcoords[0] - startingcoords[0]) ** 2 +
+	(destinationcoords[1] - startingcoords[1]) ** 2
+)
+
+print ('Distance = ', distance)
