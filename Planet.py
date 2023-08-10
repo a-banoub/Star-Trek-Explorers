5bc78dab988c3e6d8a768a7b4e0803dad779ecff
a-banoub <118484970+a-banoub@users.noreply.github.com>
2023-08-10 14:57:16 -0400

    Removed useless files

--- Planet.py
+++ Planet.py
@@ -5,14 +5,10 @@ import json
 
 class planet:
 	def __init__(self, name, game):
-		planet = self
 		
 		self.game = game		
-		
-		max_distance_ly = int ( input ("Max Distance from Current Location"))
-		
-		self.coords = MapMaker.generate_random_coordinates (max_distance_ly, self.game)
-		
+		max_distance_ly = int ( input ("Max Distance from Current Location"))		
+		self.coords = MapMaker.generate_random_coordinates (max_distance_ly, game)
 		self.properties = {
 				"class": "planet",
 				"id" : str(uuid.uuid4()),
@@ -25,13 +21,15 @@ class planet:
 			"type": "Point",
 			"coordinates": self.coords
 		}
-	
+		
 	def saveplanet(self):
+		
 		planetlog = os.path.join(self.game.dir, 'MapData.json')
 		data = {"properties": self.properties, "geometry": self.geometry}
-		
+
 		if os.path.isfile(planetlog):
 			print ("File Exists")
+			
 			with open(planetlog, "r") as file:
 				file_content = file.read()
 				planet_data = json.loads(file_content)
@@ -45,7 +43,9 @@ class planet:
 					
 				if index_to_replace is not None:
 					planet_data['features'][index_to_replace] = data
+
 				else:
 					planet_data['features'].append(data)
+				
 				with open(planetlog, "w") as file:
 					json.dump(planet_data, file)		
